546649e4c881e4c58716bdcd6f3a625f18826eb5
mubashirITU <mscs21005@itu.edu.pk>
2023-12-16 15:43:33 +0500

    second meesage

---
+++ app.py
@@ -0,0 +1,87 @@
+import streamlit as st
+from dotenv import load_dotenv
+from streamlit_chat import message
+from langchain.callbacks import get_openai_callback
+from langchain.embeddings import HuggingFaceEmbeddings
+import logging
+from utils import load_local_vectordb_using_qdrant,get_conversation_chain
+
+openapi_key = st.secrets["OPENAI_API_KEY"]
+qdrant_url = st.secrets["QDRANT_URL"]
+qdrant_api_key = st.secrets["QDRANT_API_KEY"]
+logging.basicConfig(filename="surveyGPTQdrant.log", format='%(asctime)s %(message)s', filemode='a')
+logger = logging.getLogger("surveybot.log")
+logger.setLevel(level=logging.DEBUG)
+embeddings  = HuggingFaceEmbeddings(model_name='BAAI/bge-small-en-v1.5')
+
+
+def handel_userinput(user_question,vectorstore):
+    # st.session_state.query_history.append(user_question)
+    with st.spinner('Wait for the response...'):
+        result = st.session_state.conversation({'query': user_question})
+        logger.info(f"{st.session_state.chat_history}")
+        response = result['result']
+        logger.info(f"Result={result}")
+    st.session_state.chat_history.append(user_question)
+    st.session_state.chat_history.append(f"{response} ")
+
+    response_container = st.container()
+
+    with response_container:
+        for i, messages in enumerate(st.session_state.chat_history):
+            # print(messages)
+            logger.info(f"Answer= {messages}")
+            if i % 2 == 0:
+                message(messages, is_user=True, key=str(i))
+            else:
+                message(messages, key=str(i))
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with survey paper")
+
+    st.header("Survey GPT")
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+    if "processComplete" not in st.session_state:
+        st.session_state.processComplete = None
+    if "vectorstore" not in st.session_state:
+        st.session_state.vectorstore = None
+    if "query_history" not in st.session_state:
+        st.session_state.query_history = []
+    if "memory" not in st.session_state:
+        st.session_state.memory = None
+    if "check_state" not in st.session_state:
+        st.session_state.check_state = False
+
+    with st.sidebar:
+        openai_api_key = openapi_key
+        st.sidebar.write(""" <div style="text-align: center"> The chatbot will exclusively provide information sourced from our comprehensive survey paper titled 'Semantic Segmentation of Diagnostic Medical Images: Recent Trends and Challenges.' Its primary aim is to efficiently assist users by offering insights and saving their valuable time through targeted and relevant information retrieval.</div>""", unsafe_allow_html=True)
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+    vectordb_folder_path = 'segmentation_survey'
+
+    if st.session_state.vectorstore == None:
+        vetorestore = load_local_vectordb_using_qdrant(vectordb_folder_path,embeddings,qdrant_url,qdrant_api_key)
+        st.session_state.vectorstore = vetorestore
+
+    if st.session_state.check_state == False:
+        st.session_state.conversation = get_conversation_chain(
+            st.session_state.vectorstore, openai_api_key)
+        st.session_state.check_state = True
+    st.session_state.processComplete = True
+
+    if st.session_state.processComplete == True:
+        user_question = st.chat_input("Ask Question about your files.")
+        logger.info(f"User Question= {user_question}")
+        if user_question:
+            handel_userinput(user_question, st.session_state.vectorstore)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,94 @@
+import os
+import logging
+import csv
+from langchain.prompts import PromptTemplate
+from langchain.vectorstores import FAISS,Qdrant
+from langchain.chat_models import ChatOpenAI
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.chains import RetrievalQA
+from typing import List,Tuple
+from qdrant_client import QdrantClient
+import openai
+
+logging.basicConfig(filename="surveyGPTQdrant_utils.log", format='%(asctime)s %(message)s', filemode='a')
+
+logger = logging.getLogger("survey_utils.log")
+logger.setLevel(level=logging.DEBUG)
+
+
+
+
+def _format_chat_history(chat_history: List[Tuple]) -> str:
+    buffer = ""
+    for dialogue_turn in chat_history:
+        human = "Human: " + dialogue_turn[0]
+        ai = "Assistant: " + dialogue_turn[1]
+        buffer += "\n" + "\n".join([human, ai])
+    return buffer
+
+
+
+def get_conversation_chain(vectorstore, openai_key):
+    num_chunks=10
+
+    prompt_template="""
+    As a research expert in semantic segmentation of diagnostic medical images, you have access to the following paragraphs from a survey paper on this topic. You need to answer the query by analyzing the paragraphs and extracting relevant information.
+
+    ## Paragraphs:
+    {context}
+
+    ## Query:
+    {question}
+
+    ## Answer:
+    •  Format your answer as bullet points or sentences, depending on the query.
+
+    •  Cite facts and evidence from the paragraphs to back up your answer.
+    """
+    mprompt_url = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"], validate_template=False)
+    chain_type_kwargs = {"prompt": mprompt_url}
+
+    chat_llm = ChatOpenAI(model = "gpt-3.5-turbo", openai_api_key = openai_key , temperature=0.3)
+    multiqa_retriever = MultiQueryRetriever.from_llm(llm=chat_llm, 
+                                         retriever=vectorstore.as_retriever(search_kwargs={"k": num_chunks}))
+    qa = RetrievalQA.from_chain_type(llm=chat_llm, 
+                                     chain_type="stuff", 
+                                    #  retriever=local_db.as_retriever(search_type="similarity", search_kwargs={"k": num_chunks}), 
+                                     retriever = multiqa_retriever,
+                                     chain_type_kwargs=chain_type_kwargs, 
+                                     return_source_documents=True)
+    return qa
+ 
+
+def create_new_vectorstore_qdrant( doc_list, embed_fn, COLLECTION_NAME,qdrant_url, qdrant_api_key ):
+    try:
+        qdrant = Qdrant.from_documents(
+            documents = doc_list,
+            embedding = embed_fn,
+            url=qdrant_url,
+            prefer_grpc=True,
+            api_key=qdrant_api_key,
+            collection_name=COLLECTION_NAME,
+        )
+        logger.info("Successfully created the vectordb")
+        return qdrant
+    except Exception as ex:
+        logger.exception("Vectordb Failed:"+str(ex))
+        # return JSONResponse({"Error": str(ex)})
+
+def load_local_vectordb_using_qdrant( vectordb_folder_path, embed_fn, qdrant_url, qdrant_api_key):
+    try:
+        qdrant_client = QdrantClient(
+            url=qdrant_url, 
+            prefer_grpc=True,
+            api_key=qdrant_api_key,
+        )
+        logger.info("Qdrant client loaded Successfully")
+        qdrant_store= Qdrant(qdrant_client, vectordb_folder_path, embed_fn)
+        logger.info("Successfully loaded vectordb")
+        return qdrant_store   
+    except Exception as e:
+        logger.critical(f"error while loading vectordb:'{str(e)}'")
+        raise Exception(f"error while loading vectordb:'{str(e)}'")
+    
